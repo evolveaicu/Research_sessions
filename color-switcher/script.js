593a73b10f35493141ed8666b8d6724206862a11
rachitgoyal14 <goyalrachit1410@gmail.com>
2025-11-22 00:54:52 +0530

    initial commit

---
+++ color-switcher/script.js
@@ -0,0 +1,9 @@
+function changeColor(color){
+    document.body.style.backgroundColor = color;
+}
+
+// == comparing equality (null & 0)
+// === compares in addition to comparing their data types
+
+
+// console.log(typeof(0))
---
+++ node-tutorial/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blueBright('Hello, world!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.highlight('This is a success message.'));
